9dd2745660190b7bd2d59a380f1d2f165a08fec3
Scott Woods <scott@westarete.com>
2018-03-03 22:23:06 -0500

    Parse out OSC1 wave type and octave

--- decode.rb
+++ decode.rb
@@ -7,40 +7,36 @@ class Header < BinData::Record
   string :program_name, read_length: 16
 end
 
-class Values < BinData::Record
-  endian :little
-  int16 :v1
-  int16 :v2
-  int16 :v3
-  int16 :v4
-  int16 :v5
-  int16 :v6
-  int16 :v7
-  int16 :v8
-  int16 :v9
-  int16 :v10
-  int16 :v11
-  int16 :v12
+class Oscillator1 < BinData::Record
+  skip length: 32
+  bit2 :wave_value
+  bit2 :octave_value
+
+  def wave
+    case wave_value
+      when 1 then "Square"
+      when 2 then "Triangle"
+      when 0 then "Saw"
+    end
+  end
+
+  def octave
+    octave_value == 0 ? 4 : octave_value
+  end
 end
 
+if ARGV.length == 1
+  input_file = ARGV.first
+else
+  puts "Usage: ruby decode.rb path/to/Prog_101.prog_bin"
+  exit 1
+end
 
-io = File.open("examples/empty/Prog_000.prog_bin")
+io = File.open(input_file)
 header  = Header.read(io)
-values  = Values.read(io)
+o1 = Oscillator1.read(io)
 
 p header.declaration
 p header.program_name.strip
-puts "v1  = #{values.v1}"
-puts "v2  = #{values.v2}"
-puts "v3  = #{values.v3}"
-puts "v4  = #{values.v4}"
-puts "v5  = #{values.v5}"
-puts "v6  = #{values.v6}"
-puts "v7  = #{values.v7}"
-puts "v8  = #{values.v8}"
-puts "v9  = #{values.v9}"
-puts "v10 = #{values.v10}"
-puts "v11 = #{values.v11}"
-puts "v12 = #{values.v12}"
-
-
+puts "o1.wave = #{o1.wave}"
+puts "o1.octave = #{o1.octave}"
