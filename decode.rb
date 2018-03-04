2c7c02748ffa72fe16ef8f4365200ea5d8f1a5f3
Scott Woods <scott@westarete.com>
2018-03-04 11:35:00 -0500

    Decode all controls from mixer to the left
    - Tempo
    - Octave
    - VCO1 controls
    - VCO2 controls
    - VCO2 Modulation controls
    - Mixer controls

--- decode.rb
+++ decode.rb
@@ -8,13 +8,40 @@ require 'bindata'
 class ProgramData < BinData::Record       # File position (in bytes):
   string :header, read_length: 4          # 0
   string :program_name, read_length: 16   # 4
-  uint8 :osc1_pitch                       # 20
-  uint8 :osc1_shape                       # 21
+  uint8 :vco1_pitch                       # 20
+  uint8 :vco1_shape                       # 21
+  uint8 :vco2_pitch                       # 22
+  uint8 :vco2_shape                       # 23
+  uint8 :vco2_cross_mod_depth             # 24
+  uint8 :vco2_pitch_eg_int                # 25
+  uint8 :mixer_vco1                       # 26
+  uint8 :mixer_vco2                       # 27
+  uint8 :mixer_noise                      # 28
 
-  skip length: 30                         # 22
+  skip length: 23                         # 29
   
-  bit2 :osc1_wave                         # 52 + 0 bits
-  bit2 :osc1_octave                       # 52 + 2 bits
+  bit2 :vco1_wave                         # 52 + 0 bits
+  bit2 :vco1_octave                       # 52 + 2 bits
+  bit4 :skip1                             # 52 + 4 bits
+  bit2 :vco2_wave                         # 53 + 0 bits
+  bit2 :vco2_octave                       # 53 + 2 bits
+  bit4 :skip2                             # 53 + 4 bits
+  
+  skip length: 1                          # 54
+
+  bit6 :skip3                             # 55 + 0 bits
+  bit1 :vco2_ring                         # 55 + 6 bits
+  bit1 :vco2_sync                         # 55 + 7 bits
+
+  skip length: 17                         # 56
+
+  bit5 :skip4                             # 73 + 0 bits
+  bit3 :octave                            # 73 + 5 bits
+
+  skip length: 26                         # 74
+
+  uint16le :tempo                         # 100
+
 end
 
 # A class that knows how to convert the raw program data values into more
@@ -38,20 +65,100 @@ class Program
     @data.program_name
   end
 
-  def osc1_wave
-    case @data.osc1_wave
-      when 1 then :square
-      when 2 then :triangle
-      when 0 then :saw
+  def tempo
+    @data.tempo / 10.0
+  end
+
+  def octave
+    normalize_octave(@data.octave)
+  end
+
+  # VCO1
+
+  def vco1_octave
+    normalize_octave(@data.vco1_octave)
+  end
+
+  def vco1_wave
+    normalize_wave(@data.vco1_wave)
+  end
+
+  def vco1_pitch
+    normalize_balanced_knob(@data.vco1_pitch)
+  end
+
+  def vco1_shape
+    normalize_positive_knob(@data.vco1_shape)
+  end
+
+  # VCO2
+
+  def vco2_octave
+    normalize_octave(@data.vco2_octave)
+  end
+
+  def vco2_wave
+    normalize_wave(@data.vco2_wave)
+  end
+
+  def vco2_pitch
+    normalize_balanced_knob(@data.vco2_pitch)
+  end
+
+  def vco2_shape
+    normalize_positive_knob(@data.vco2_shape)
+  end
+
+  def vco2_cross_mod_depth
+    normalize_positive_knob(@data.vco2_cross_mod_depth)
+  end
+
+  def vco2_pitch_eg_int
+    normalize_balanced_knob(@data.vco2_pitch_eg_int)
+  end
+
+  def vco2_sync
+    on_off_switch(@data.vco2_sync)
+  end
+
+  def vco2_ring
+    on_off_switch(@data.vco2_ring)
+  end
+
+  # Mixer
+
+  def mixer_vco1
+    normalize_positive_knob(@data.mixer_vco1)
+  end
+
+  def mixer_vco2
+    normalize_positive_knob(@data.mixer_vco2)
+  end
+
+  def mixer_noise
+    normalize_positive_knob(@data.mixer_noise)
+  end
+
+  private
+
+  def normalize_wave(value)
+    case value
+      when 0 then :square
+      when 1 then :triangle
+      when 2 then :saw
     end
   end
 
-  def osc1_octave
-    @data.osc1_octave == 0 ? 4 : @data.osc1_octave
+  def normalize_octave(value)
+    value + 1
   end
 
-  def osc1_pitch
-    normalized = @data.osc1_pitch - 128
+  def normalize_positive_knob(value)
+    value / 255.0
+  end
+
+  def normalize_balanced_knob(value)
+    normalized = value - 128
     if normalized < 0
       normalized /= 128.0
     else
@@ -59,8 +166,12 @@ class Program
     end
   end
 
-  def osc1_shape
-    @data.osc1_shape / 255.0
+  def on_off_switch(value)
+    case value
+      when 0 then :off
+      when 1 then :on
+      else nil
+    end
   end
 
 end
@@ -80,10 +191,29 @@ class ProgramFormatter
   def to_s
     <<-EOF
     Program name = #{program_name}
-    OSC1 Wave = #{@program.osc1_wave}
-    OSC1 Octave = #{@program.osc1_octave}
-    OSC1 Pitch = #{osc1_pitch}
-    OSC1 Shape = #{osc1_shape}
+
+    Tempo = #{@program.tempo} BPM
+    Octave = #{@program.octave}
+
+    VCO1 Wave = #{@program.vco1_wave}
+    VCO1 Octave = #{@program.vco1_octave}
+    VCO1 Pitch = #{vco1_pitch}
+    VCO1 Shape = #{vco1_shape}
+
+    VCO2 Wave = #{@program.vco2_wave}
+    VCO2 Octave = #{@program.vco2_octave}
+    VCO2 Pitch = #{vco2_pitch}
+    VCO2 Shape = #{vco2_shape}
+
+    VCO2 Cross Mod Depth = #{vco2_cross_mod_depth}
+    VCO2 Pitch EG Int = #{vco2_pitch_eg_int}
+    VCO2 Sync = #{@program.vco2_sync}
+    VCO2 Ring = #{@program.vco2_ring}
+
+    Mixer for VCO1 = #{mixer_vco1}
+    Mixer for VCO2 = #{mixer_vco2}
+    Mixer for Noise = #{mixer_noise}
+
     EOF
   end
 
@@ -91,19 +221,57 @@ class ProgramFormatter
     @program.name.strip
   end
 
-  def osc1_pitch
-    if @program.osc1_pitch.nil?
+  def vco1_pitch
+    format_cents(@program.vco1_pitch, 1200)
+  end
+
+  def vco1_shape
+    format_percent(@program.vco1_shape)
+  end
+
+  def vco2_pitch
+    format_cents(@program.vco2_pitch, 1200)
+  end
+
+  def vco2_shape
+    format_percent(@program.vco2_shape)
+  end
+
+  def vco2_cross_mod_depth
+    format_percent(@program.vco2_cross_mod_depth)
+  end
+
+  def vco2_pitch_eg_int
+    format_cents(@program.vco2_pitch_eg_int, 4800)
+  end
+
+  def mixer_vco1
+    format_percent(@program.mixer_vco1)
+  end
+
+  def mixer_vco2
+    format_percent(@program.mixer_vco2)
+  end
+
+  def mixer_noise
+    format_percent(@program.mixer_noise)
+  end
+
+  private
+
+  def format_cents(value, max_value)
+    if value.nil?
       nil
     else
-      "#{(@program.osc1_pitch * 1200).round}C"
+      "#{(value * max_value).round}C"
     end
   end
 
-  def osc1_shape
-    if @program.osc1_shape.nil?
+  def format_percent(value)
+    if value.nil?
       nil
     else
-      "#{(@program.osc1_shape * 100).round}%"
+      "#{(value * 100).round}%"
     end
   end
 
